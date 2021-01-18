bd10db2d7984dac946d873faeee427128fd4556f
Jasson Cordones <jasson.cordones@gmail.com>
2021-01-18 09:24:55 -0400

    Added support for multiple lines and removed printing new line

--- get-quote.py
+++ get-quote.py
@@ -1,15 +1,17 @@
 import random
+import sys
+def primary(n = sys.argv[1]):
 
-def primary():
   #print("Keep it logically awesome.")
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
   last = len(quotes) - 1
-  rnd = random.randint(0,last)
-
-  print(quotes[rnd])
+  
+  for i in range(int(n)):
+    rnd = random.randint(0,last)
+    print(quotes[rnd], end = '')
 
 if __name__== "__main__":
   primary()
